6426a5b65372b5a9b8d1ba8cca2eb4b26016d8f1
Raju Gandhi <raju.gandhi+github@gmail.com>
2012-07-23 09:58:05 -0400

    Initial project skeleton

---
+++ src/com_looselytyped_99problems/core.clj
@@ -0,0 +1,6 @@
+(ns com-looselytyped-99problems.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/com_looselytyped_99problems/core_test.clj
@@ -0,0 +1,7 @@
+(ns com-looselytyped-99problems.core-test
+  (:use clojure.test
+        com-looselytyped-99problems.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
